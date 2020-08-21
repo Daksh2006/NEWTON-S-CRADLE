4388b53d92a115a25c286f4d23ab7c0233e6d455
Daksh2006 <68325624+Daksh2006@users.noreply.github.com>
2020-08-21 15:02:49 +0530

    Update Chain.js

--- Chain.js
+++ Chain.js
@@ -4,7 +4,7 @@ class Chain{
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
-            length: 10
+            length: 400
         }
         this.pointB = pointB
         this.pointA = this.pointA
@@ -17,21 +17,10 @@ class Chain{
     }
 
     display(){
-        if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             stroke(48,22,8);
-            if (pointA.x < 220){
-                strokeWeight(8);
-                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
-                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
-                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
-            } else {
                 strokeWeight(4);
-                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
-                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
-                image(this.sling3,pointA.x+25,pointA.y-10,15,30);
-            }
+                line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
-            }  
-        }       
+            }       
