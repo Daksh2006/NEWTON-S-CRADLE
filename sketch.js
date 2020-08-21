9f34bf512ac040c2b6bbb5cb9fa80d6fa4715b5b
Daksh2006 <68325624+Daksh2006@users.noreply.github.com>
2020-08-21 15:02:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -19,15 +18,15 @@ function setup() {
 	world = engine.world
 
 	//Create the Bodies Here.
-	b1 = new Balls(250,400,40,40);
-	b2 = new Balls(350,400,40,40);
-	b3 = new Balls(450,400,40,40);
-	b4 = new Balls(550,400,40,40);
+	b1 = new Balls(250,400,60,60);
+	b2 = new Balls(350,400,60,60);
+	b3 = new Balls(450,400,60,60);
+	b4 = new Balls(550,400,60,60);
 	stand= createSprite(400,100,400,20);
-	s1= new Chain(b1.body,{x:250, y:400});
-	s2= new Chain(b2.body,{x:350, y:400});
-	s3= new Chain(b3.body,{x:450, y:400});
-	s4= new Chain(b4.body,{x:550, y:400});
+	s1= new Chain(b1.body,{x:250, y:110});
+	s2= new Chain(b2.body,{x:350, y:110});
+	s3= new Chain(b3.body,{x:450, y:110});
+	s4= new Chain(b4.body,{x:550, y:110});
 	Engine.run(engine);
   
 }
@@ -41,12 +40,15 @@ function draw() {
   b3.display();
   b4.display();
   drawSprites();
+  s1.display();
+  s2.display();
+  s3.display();
+  s4.display();
  
 }
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
-		Matter.Body.applyForce(b1.body,b1.body.position,{x:-300,y:380});
+		Matter.Body.applyForce(b1.body,b1.body.position,{x:-700,y:380});
 	 }
    }
-
 
